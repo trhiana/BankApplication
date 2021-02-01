be12677056d10a5285a8a0129944241680ae8b14
trhiana <trhiana@rocketmail.com>
2021-02-01 17:45:30 +0300

    Updated README.md

--- BankApplication.java
+++ BankApplication.java
@@ -18,6 +18,7 @@ class BankAccount{
     String customerId;
 
     void deposit(int amount){
+        // Only perform this if amount entered is not zero
         if (amount != 0){
             balance += amount;
             previousTransaction = amount;
@@ -25,9 +26,14 @@ class BankAccount{
     }
 
     void withdraw(int amount){
+        // Only perform this if amount entered is not zero
         if (amount != 0){
             balance -= amount;
             previousTransaction = -amount;
         }
+    }
+
+    void getPreviousTransaction(){
+
     }
 }
--- README.md
+++ README.md
@@ -1,4 +1,5 @@
 # Bank Application
+A simple banking application to check the balance, deposit, withdraw, e.t.c on a bank account.  
 
 ## Technologies
 * Java 8
