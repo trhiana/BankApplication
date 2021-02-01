a5db9243ec73a4fcb4b79bf4a76c78eb990ffeb5
trhiana <trhiana@rocketmail.com>
2021-02-01 18:11:36 +0300

    Added getPreviousTransaction and showMenu methods

--- BankApplication.java
+++ BankApplication.java
@@ -17,23 +17,37 @@ class BankAccount{
     String customerName;
     String customerId;
 
+    // Updating the account with the deposited amount.
     void deposit(int amount){
-        // Only perform this if amount entered is not zero
+        // Only perform this if amount entered is not zero.
         if (amount != 0){
             balance += amount;
             previousTransaction = amount;
         }
     }
 
+    // Updating the account with the withdrawn amount.
     void withdraw(int amount){
-        // Only perform this if amount entered is not zero
+        // Only perform this if amount entered is not zero.
         if (amount != 0){
             balance -= amount;
             previousTransaction = -amount;
         }
     }
 
+    // Shows the account holder the amount deposited or withdrawn in the most recent transaction.
     void getPreviousTransaction(){
+        if (previousTransaction > 0){
+            // If the account holder has deposited
+            System.out.println("Amount deposited: " + previousTransaction);
+        } else if (previousTransaction < 0){
+            // If the account holder has withdrawn
+            System.out.println("Amount withdrawn: " + previousTransaction);
+        } else{
+            // If there has been no transaction
+            System.out.println("There was no transaction");
+        }
 
+        
     }
 }
