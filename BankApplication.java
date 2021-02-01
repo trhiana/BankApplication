875f23efd8b62251fd4554cb37c82b79df83bc58
trhiana <trhiana@rocketmail.com>
2021-02-01 17:34:02 +0300

    Added deposit and withdraw functions

--- BankApplication.java
+++ BankApplication.java
@@ -10,3 +10,24 @@
 public class BankApplication{
 
 }
+
+class BankAccount{
+    int balance;
+    int previousTransaction;
+    String customerName;
+    String customerId;
+
+    void deposit(int amount){
+        if (amount != 0){
+            balance += amount;
+            previousTransaction = amount;
+        }
+    }
+
+    void withdraw(int amount){
+        if (amount != 0){
+            balance -= amount;
+            previousTransaction = -amount;
+        }
+    }
+}
