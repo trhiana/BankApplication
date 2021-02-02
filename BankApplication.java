7fb59352e8ade727f296e585d2b242ba298bfd88
trhiana <trhiana@rocketmail.com>
2021-02-02 15:21:25 +0300

    Added showMenu method

--- BankApplication.java
+++ BankApplication.java
@@ -2,10 +2,11 @@
  * @author Rhiana Thelemaque
  */
 
+
 /**
  * Import packages
  */
-
+import java.util.Scanner;
 
 public class BankApplication{
 
@@ -16,6 +17,12 @@ class BankAccount{
     int previousTransaction;
     String customerName;
     String customerId;
+
+    // Bank account information
+    BankAccount(String cname, String cid){
+        customerName = cname;
+        customerId = cid;
+    }
 
     // Updating the account with the deposited amount.
     void deposit(int amount){
@@ -42,12 +49,81 @@ class BankAccount{
             System.out.println("Amount deposited: " + previousTransaction);
         } else if (previousTransaction < 0){
             // If the account holder has withdrawn
-            System.out.println("Amount withdrawn: " + previousTransaction);
+            System.out.println("Amount withdrawn: " + Math.abs(previousTransaction));
         } else{
             // If there has been no transaction
-            System.out.println("There was no transaction");
-        }
+            System.out.println("There was no transaction.");
+        }        
+    }
 
-        
+    // Displaying welcome message and menu
+    void showMenu(){
+        char option = '\0';
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("Welcome " + customerName);
+        System.out.println("Your ID is " + customerId); 
+        System.out.println("\n");
+        System.out.println("A. Check Balance.");
+        System.out.println("B. Deposit.");
+        System.out.println("C. Withdraw.");
+        System.out.println("D. Previous Transaction.");
+        System.out.println("E. Exit.");
+
+        // Account holder selects option from the above 
+        do{
+            System.out.println("=============================================================");
+            System.out.println("Enter an option: ");
+            System.out.println("=============================================================");
+            option = scanner.next().charAt(0);
+            System.out.println("\n");
+
+            switch(option){
+                // Check balance
+                case 'A':
+                    System.out.println("-----------------------------------------------------");
+                    System.out.println("Account balance: " + balance);
+                    System.out.println("-----------------------------------------------------");
+                    System.out.println("\n");
+                    break;
+                
+                // Deposit
+                case 'B':
+                    System.out.println("-----------------------------------------------------");
+                    System.out.println("Enter amount to deposit: ");
+                    System.out.println("-----------------------------------------------------");
+                    int amount = scanner.nextInt();
+                    deposit(amount);
+                    System.out.println("\n");
+                    break;
+                
+                // Withdraw
+                case 'C':
+                    System.out.println("-----------------------------------------------------");
+                    System.out.println("Enter amount to withdraw: ");
+                    System.out.println("-----------------------------------------------------");
+                    int amountTwo = scanner.nextInt();
+                    deposit(amountTwo);
+                    System.out.println("\n");
+                    break;
+                
+                // Previous transaction
+                case 'D':
+                    System.out.println("-----------------------------------------------------");
+                    getPreviousTransaction();
+                    System.out.println("-----------------------------------------------------");
+                    System.out.println("\n");
+                    break;
+                
+                // If input is not in the given options
+                default:
+                    System.out.println("Invalid selection! Please enter another option.");
+                    break;
+
+            }
+
+        } while (option != 'E');
+
+        System.out.println("Thank you for using our services!");
     }
 }
